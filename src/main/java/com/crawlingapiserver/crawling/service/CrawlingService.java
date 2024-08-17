a361f108efdea190cab13fde7d035a2f7784da4e
dpound@naver.com <dpound@naver.com>
2024-08-17 10:19:48 +0900

    크롤링으로 html 전체 문서 긁어오는 로직까지 완성

--- src/main/java/com/crawlingapiserver/crawling/service/CrawlingService.java
+++ src/main/java/com/crawlingapiserver/crawling/service/CrawlingService.java
@@ -2,6 +2,10 @@ package com.crawlingapiserver.crawling.service;
 
 import com.crawlingapiserver.crawling.model.CommandModel;
 import lombok.extern.log4j.Log4j2;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedWriter;
@@ -20,26 +24,68 @@ public class CrawlingService {
      * 최종 목적 uri 실행시켜주는 최종 서비스 메소드
      * */
     public List<String> runCrawling(CommandModel commandModel, List<String> finalTargetList, Connection connection){
-        Boolean DbSwitch = false;
 
-        if(connection != null){
-            DbSwitch = true;
-            log.info("DB Direct Insert Mode ON");
+        try {
+            Boolean DbSwitch = false;
+
+            if(connection != null){
+                DbSwitch = true;
+                log.info("DB Direct Insert Mode ON");
+            }
+
+            // DB 체크
+            if(DbSwitch){
+
+            }
+
+            for (String crawlingUri : finalTargetList) {
+                Document doc = Jsoup.connect(crawlingUri).get();
+                Elements posts = doc.getElementsByTag("#bo_v");
+
+                log.info(doc.toString());
+                if(isPageNotFound(doc)){
+                    log.info("Page Not Found");
+                }else{
+                    for (Element post : posts) {
+                        System.out.println(post.text());
+                    }
+
+                }
+
+            }
+
+
+
+            Boolean textFileBoolean = commandModel.getDatabase().getMakeInsertTextFile();
+            if(textFileBoolean != null && textFileBoolean){
+                // 텍스트파일 생성
+                writeInsertTextFile(commandModel.getDatabase().getInsertTextFilePhysicalSavePath(), new ArrayList<String>());
+            }
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
-        // DB 체크
-        if(DbSwitch){
-
-        }
-
-        Boolean textFileBoolean = commandModel.getDatabase().getMakeInsertTextFile();
-        if(textFileBoolean != null && textFileBoolean){
-            // 텍스트파일 생성
-            writeInsertTextFile(commandModel.getDatabase().getInsertTextFilePhysicalSavePath(), new ArrayList<String>());
-        }
 
         return null;
     }
+
+
+
+
+
+    /**
+     *
+     * 이미지 처리 방식
+     * 1. src 경로를 원하는대로 수정해주기 (UUID 옵션 선택가능)
+     * 2. base64 방식으로 변환해서 데이터 저장해주기
+     * */
+    public void imageProcessing(CommandModel commandModel, Element element){
+
+    }
+
 
     /**
      * 만들어진 쿼리 리스트를 텍스트파일로 생성해서 저장해줌,
@@ -55,5 +101,22 @@ public class CrawlingService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isPageNotFound(Document doc) {
+        // 페이지에 "글이 존재하지 않습니다" 메시지가 포함된 경우 처리
+        Elements scriptElements = doc.select("script");
+        for (Element script : scriptElements) {
+            if (script.toString().contains("alert(\"글이 존재하지 않습니다.\"")) {
+                return true;
+            }
+        }
+        Elements noScriptElements = doc.select("noscript");
+        for (Element noScript : noScriptElements) {
+            if (noScript.toString().contains("글이 존재하지 않습니다.")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
